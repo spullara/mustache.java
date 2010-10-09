3331943860286dcf43f8895f20669a73846fbe6c
Pieter van de Bruggen <pvande@gmail.com>
2010-10-09 16:13:20 -0700

    Replacing hand-rolled test script.

--- scripts/mustache-gem.rb
+++ scripts/mustache-gem.rb
@@ -4,74 +4,47 @@ require 'rubygems'
 require 'mustache'
 require 'tmpdir'
 require 'yaml'
-
-begin
-  require 'Win32/Console/ANSI' if RUBY_PLATFORM =~ /win32/
-rescue LoadError
-  raise 'You must gem install win32console to use color on Windows'
-end
-
-def colorize(text, color)
-  return "\e[#{color}m#{text}\e[0m"
-end
-
-def partials(*file)
-  return File.join(File.dirname(__FILE__), 'partials', *file)
-end
-alias :partial :partials
-
-Mustache.template_path = partials
-failures = []
+require 'test/unit'
 
 Dir[File.join(File.dirname(__FILE__), '..', 'specs', '*.yml')].each do |file|
   spec = YAML.load_file(file)
-  puts
-  puts "Running tests in #{ File.basename(file) }\n"
 
-  failed = spec['tests'].reject do |test|
-    test[:file] = file
+  Class.new(Test::Unit::TestCase) do
+    define_method :name do
+      File.basename(file).sub(/^./, &:upcase)
+    end
 
-    begin
-      print "Testing #{ test['name'].downcase }... "
+    def setup
+      @partials = File.join(File.dirname(__FILE__), '..', 'partials')
+      Dir.mkdir(@partials)
+      Mustache.template_path = @partials
+    end
 
-      if test.has_key?('partials')
-        Dir.mkdir(partials)
-        test['partials'].each do |file, content|
-          File.open(partial("#{file}.mustache"), 'w') { |f| f.print(content) }
+    def teardown
+      Dir[File.join(@partials, '*')].each { |file| File.delete(file) }
+      Dir.rmdir(@partials)
+    end
+
+    def write_partials(partials)
+      partials.each do |name, content|
+        File.open(File.join(@partials, "#{name}.mustache"), 'w') do |f|
+          f.print(content)
         end
       end
+    end
 
-      test['output'] = Mustache.render(test['template'], test['data'])
-      if test['output'] == test['expected']
-        print colorize("PASSED\n", 32)
-        true
-      else
-        puts colorize("FAILED!", 31)
+    spec['tests'].each do |test|
+      define_method :"test: #{test['name']}" do
+        write_partials test['partials'] if test.has_key? 'partials'
 
-        puts "Given the template:", '', colorize(test['template'], '4;33')
-        if test.has_key?('partials')
-          puts "Partials:", ''
-          test['partials'].each do |file, content|
-            puts colorize("#{file}.mustache", 43)
-            puts colorize(content, '4;33')
-          end
-        end
-        puts "And data:", '',           test['data'].inspect, ''
-        puts "Mustache rendered:", '',  colorize(test['output'], '4;31')
-        puts "Instead of:", '',         colorize(test['expected'], '4;32')
-        false
+        result = Mustache.render(test['template'], test['data'])
+        assert_equal test['expected'], result, <<-MSG.gsub(/^\s+/, '')
+          Test: #{test['name']}
+          Data: #{test['data'].inspect}
+          Template: #{test['template'].inspect}
+          Partials: #{(test['partials'] || {}).inspect}
+        MSG
       end
-    ensure
-      Dir[File.join(partials, '*')].each { |file| File.delete(file) }
-      Dir.rmdir(partials) if File.exists?(partials)
     end
   end
-
-  failures << failed
-end
-
-puts
-puts colorize("Final Results: #{failures.inject(0) { |a,e| a + e.length }} failures", 0)
-failures.each do |e|
-  e.each { |e| puts "#{ File.basename(e[:file]) }: Failed #{ e['name'] }" }
 end
