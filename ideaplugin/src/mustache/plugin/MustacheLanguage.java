982aa52547698e5abe40c032f0d8eb7415daef22
Sam Pullara <spullara@yahoo.com>
2011-06-04 12:37:03 -0700

    playing around with intellij plugin for mustache.

---
+++ ideaplugin/src/mustache/plugin/MustacheLanguage.java
@@ -0,0 +1,17 @@
+package mustache.plugin;
+
+import com.intellij.lang.Language;
+
+/**
+ * TODO: Edit this
+ * <p/>
+ * User: sam
+ * Date: 6/4/11
+ * Time: 12:32 PM
+ */
+public class MustacheLanguage extends Language {
+  public MustacheLanguage() {
+    super("Mustache", "text/mustache", "text/plain");
+  }
+
+}
