bc489c317d7899ca14f8a0d39278b09313b5d8d3
Sam Pullara <spullara@twitter.com>
2012-04-13 15:21:16 -0700

    now only a small performance regression from the original flawed model. need to determine if we also need to cache.

--- compiler/src/main/java/com/github/mustachejava/reflect/GuardedWrapper.java
+++ compiler/src/main/java/com/github/mustachejava/reflect/GuardedWrapper.java
@@ -12,6 +12,9 @@ import com.github.mustachejava.util.Wrapper;
  */
 public class GuardedWrapper implements Wrapper {
   protected final List<? extends Predicate<Object[]>> guard;
+  private Predicate[] predicates;
+  private int hashCode;
+  private int size;
 
   public GuardedWrapper(List<? extends Predicate<Object[]>> guard) {
     this.guard = guard;
@@ -24,10 +27,44 @@ public class GuardedWrapper implements Wrapper {
   }
 
   protected void guardCall(Object[] scopes) throws GuardException {
-    for (Predicate<Object[]> predicate : guard) {
-      if (!predicate.apply(scopes)) {
-        throw new GuardException();
+    if (predicates == null) {
+      size = guard.size();
+      predicates = new Predicate[size];
+      int size = guard.size();
+      for (int i = 0; i < size; i++) {
+        Predicate<Object[]> predicate = guard.get(i);
+        predicates[i] = predicate;
+        if (!predicate.apply(scopes)) {
+          throw new GuardException();
+        }
+      }
+    } else {
+      int length = size;
+      for (int i = 0; i < length; i++) {
+        if (!predicates[i].apply(scopes)) {
+          throw new GuardException();
+        }
       }
     }
+  }
+
+  @Override
+  public int hashCode() {
+    if (hashCode == 0) {
+      for (Predicate<Object[]> predicate : guard) {
+        hashCode += hashCode * 43 + predicate.hashCode();
+      }
+      if (hashCode == 0) hashCode = 1;
+    }
+    return hashCode;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o instanceof GuardedWrapper) {
+      GuardedWrapper other = (GuardedWrapper) o;
+      return (guard == null && other.guard == null) || other.guard.equals(guard);
+    }
+    return false;
   }
 }
