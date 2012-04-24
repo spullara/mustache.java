7dc7bcd155eeb8e7af10bd83b1f420bb683604d0
Matt Cheely <mcheely@lulu.com>
2012-04-24 19:59:00 -0400

    Fix problems with dot notation misses on last element in path

--- compiler/src/test/java/com/github/mustachejava/DotNotationTest.java
+++ compiler/src/test/java/com/github/mustachejava/DotNotationTest.java
@@ -1,56 +1,77 @@
 package com.github.mustachejava;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import java.io.*;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
+
+import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class DotNotationTest {
 
-  private static final String TEMPLATE = "{{container1.container2.target}}";
+  private static final String EARLY_MISS_TEMPLATE = "{{container1.container2.target}}";
+  private static final String LAST_ELEMENT_MISS_TEMPLATE = "{{container1.nothing}}";
   
   private static final class ModelObject {
+      @SuppressWarnings("unused")
       public Object getContainer2() {
           return null;
       }
   }
   
-  private Mustache mustache;
+  private MustacheFactory factory;
+  private Map<String, Object> mapModel;
+  private Map<String, Object> objectModel;
   
   @Before
   public void setUp() {
-      MustacheFactory factory = new DefaultMustacheFactory();
-      Reader reader = new StringReader(TEMPLATE);
-      mustache = factory.compile(reader, "template");
+      factory = new DefaultMustacheFactory();
+      
+      mapModel = new HashMap<String, Object>();
+      Map<String, Object> container1 = new HashMap<String, Object>();
+      mapModel.put("container1", container1);
+      
+      objectModel = new HashMap<String, Object>();
+      objectModel.put("container1", new ModelObject());
   }
   
 
   @Test
   public void testIncompleteMapPath() {
-      Map<String, Object> model = new HashMap<String, Object>();
-      Map<String, Object> container1 = new HashMap<String, Object>();
-      model.put("container1", container1);
-      StringWriter writer = new StringWriter();
-      mustache.execute(writer, model);
-      
-      assertEquals("", writer.toString());
+      testMiss(mapModel, EARLY_MISS_TEMPLATE);
+  }
+  
+  @Test
+  public void testAlmostCompleteMapPath() {
+      testMiss(mapModel, LAST_ELEMENT_MISS_TEMPLATE);
+  }
+  
+  @Test
+  public void testIncompleteObjectPath() {
+      testMiss(objectModel, EARLY_MISS_TEMPLATE);
   }
 
   @Test
-  public void testIncompleteObjectPath() {
-      Map<String, Object> model = new HashMap<String, Object>();
-      Object container1 = new ModelObject();
-      model.put("container1", container1);
+  public void testAlmostCompleteObjectPath() {
+      testMiss(objectModel, LAST_ELEMENT_MISS_TEMPLATE);
+  }
+  
+  private void testMiss(Object model, String template) {
+      Mustache mustache = compile(template);
       StringWriter writer = new StringWriter();
       mustache.execute(writer, model);
       
       assertEquals("", writer.toString());
   }
 
+  private Mustache compile(String template) {
+      Reader reader = new StringReader(template);
+      return factory.compile(reader, "template");
+  }
+      
+  
 }
