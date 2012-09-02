958198b25c6f47258e8bec1e1bbbb999afe3272a
Sam Pullara <spullara@twitter.com>
2012-09-02 09:56:19 -0700

    add a scalaobjecthandler that doesnt require the twitter libraries

--- compiler/src/test/scala/com/twitter/mustache/ObjectHandlerTest.scala
+++ compiler/src/test/scala/com/twitter/mustache/ObjectHandlerTest.scala
@@ -3,12 +3,12 @@ package com.twitter.mustache
 import com.github.mustachejava.DefaultMustacheFactory
 import com.twitter.util.FuturePool
 import java.io.{StringWriter, StringReader}
-import java.util.concurrent.Executors
+import java.util.concurrent.{Callable, Executors}
 import org.junit.{Assert, Test}
 
 class ObjectHandlerTest {
   @Test
-  def testHandler() {
+  def testTwitterHandler() {
     val pool = Executors.newCachedThreadPool()
     val futurePool = FuturePool(pool)
     val mf = new DefaultMustacheFactory()
@@ -34,6 +34,42 @@ class ObjectHandlerTest {
         val futureWorld = futurePool {
           "thanks for all the fish"
         }
+        lazy val test = false
+        val map = Map(("value", "test"))
+        val num = 1
+      })
+    })
+    // You must use close if you use concurrent latched writers
+    writer.close()
+    Assert.assertEquals("Hello, world!?!\nGoodbye, thanks for all the fish!!?test\n", sw.toString)
+  }
+
+  @Test
+  def testScalaHandler() {
+    val pool = Executors.newCachedThreadPool()
+    val mf = new DefaultMustacheFactory()
+    mf.setObjectHandler(new TwitterObjectHandler)
+    mf.setExecutorService(pool)
+    val m = mf.compile(
+      new StringReader("{{#list}}{{optionalHello}}, {{futureWorld}}!" +
+              "{{#test}}?{{/test}}{{^test}}!{{/test}}{{#num}}?{{/num}}{{^num}}!{{/num}}" +
+              "{{#map}}{{value}}{{/map}}\n{{/list}}"),
+      "helloworld"
+    )
+    val sw = new StringWriter
+    val writer = m.execute(sw, new {
+      val list = Seq(new {
+        lazy val optionalHello = Some("Hello")
+        val futureWorld = pool.submit(new Callable[String] {
+          def call(): String = "world"
+        })
+        val test = true
+        val num = 0
+      }, new {
+        val optionalHello = Some("Goodbye")
+        val futureWorld = pool.submit(new Callable[String] {
+          def call(): String = "thanks for all the fish"
+        })
         lazy val test = false
         val map = Map(("value", "test"))
         val num = 1
