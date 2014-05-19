1d8713bf2078a72a78558b584e098e6e325865ce
Mark Derricutt <mark.derricutt@smxemail.com>
2014-05-19 12:23:22 +1200

    Added basic Maven Plugin to validate mustache templates

---
+++ mustache-maven-plugin/src/main/java/com/github/spullara/mustache/mojo/MustacheValidationMojo.java
@@ -0,0 +1,65 @@
+package com.github.spullara.mustache.mojo;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.MustacheException;
+import com.github.mustachejava.MustacheFactory;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.codehaus.plexus.compiler.util.scan.InclusionScanException;
+import org.codehaus.plexus.compiler.util.scan.SimpleSourceInclusionScanner;
+import org.codehaus.plexus.compiler.util.scan.SourceInclusionScanner;
+import org.codehaus.plexus.compiler.util.scan.StaleSourceScanner;
+import org.codehaus.plexus.compiler.util.scan.mapping.SuffixMapping;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@Mojo(name = "validate", defaultPhase = LifecyclePhase.PROCESS_RESOURCES)
+public class MustacheValidationMojo extends AbstractMojo {
+
+  @Parameter(defaultValue = "src/main/resources")
+  private File sourceDirectory;
+
+  @Parameter(defaultValue = "target/classes")
+  private File outputDirectory;
+
+  @Parameter(defaultValue = "false")
+  private boolean includeStale;
+
+  public void execute() throws MojoExecutionException, MojoFailureException {
+
+    SourceInclusionScanner scanner = includeStale
+      ? new StaleSourceScanner(1024, Collections.singleton("**/*.mustache"), Collections.<String>emptySet())
+      : new SimpleSourceInclusionScanner(Collections.singleton("**/*.mustache"), Collections.<String>emptySet());
+
+    scanner.addSourceMapping(new SuffixMapping(".mustache", new HashSet(Arrays.asList(".mustache"))));
+
+    MustacheFactory mustacheFactory = new DefaultMustacheFactory();
+    try {
+      Set<File> files = scanner.getIncludedSources(sourceDirectory, outputDirectory);
+
+      for (File file : files) {
+        try {
+          mustacheFactory.compile(new FileReader(file), file.getAbsolutePath());
+        } catch (MustacheException e) {
+          throw new MojoFailureException(e.getMessage(), e);
+        }
+      }
+
+    } catch (InclusionScanException e) {
+      throw new MojoExecutionException(e.getMessage());
+    } catch (FileNotFoundException e) {
+      throw new MojoExecutionException(e.getMessage());
+    }
+
+  }
+}
