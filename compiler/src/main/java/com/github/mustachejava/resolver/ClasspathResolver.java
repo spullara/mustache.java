63074672b2b2babf6639cd491789b6c2f160aa4b
Sam Pullara <spullara@gmail.com>
2021-07-02 13:49:54 -0700

    undo import *

--- compiler/src/main/java/com/github/mustachejava/resolver/ClasspathResolver.java
+++ compiler/src/main/java/com/github/mustachejava/resolver/ClasspathResolver.java
@@ -2,7 +2,11 @@ package com.github.mustachejava.resolver;
 
 import com.github.mustachejava.MustacheResolver;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URI;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
