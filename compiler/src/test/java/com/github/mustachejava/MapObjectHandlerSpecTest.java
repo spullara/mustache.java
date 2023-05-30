a46521be7630a906f867f00cf473b82201a0a588
Henoch Yehayes <hyehayes@google.com>
2023-05-30 07:51:02 -0400

    map spec now runs all tests

--- compiler/src/test/java/com/github/mustachejava/MapObjectHandlerSpecTest.java
+++ compiler/src/test/java/com/github/mustachejava/MapObjectHandlerSpecTest.java
@@ -6,14 +6,6 @@ import com.github.mustachejava.reflect.SimpleObjectHandler;
 import org.junit.Test;
 
 public class MapObjectHandlerSpecTest extends SpecTest {
-
-  @Override
-  @Test
-  public void lambdas() {
-    // MapObjectHandler does not support lambdas as that would require looking up the value of
-    // object methods in the Map.
-  }
-
   @Override
   protected DefaultMustacheFactory createMustacheFactory(JsonNode test) {
     DefaultMustacheFactory mf = super.createMustacheFactory(test);
